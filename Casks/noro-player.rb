6278f83c8bd6af36e7c29d63ac5bf8316c22c52e
0oAstro <79555780+0oAstro@users.noreply.github.com>
2026-04-02 06:53:25 +0530

    Update noro-player sha256 for icon release

--- Casks/noro-player.rb
+++ Casks/noro-player.rb
@@ -1,6 +1,6 @@
 cask "noro-player" do
   version "1.0.0"
-  sha256 "b27d2032d0441269c92b14cb66120adbab3cdbaac8e748bcb9281cd94bf8744d"
+  sha256 "04dd9086fd5f6b04cf62d263ead4845ff483d76a657898ebed99e00828e4c981"
 
   url "https://github.com/0oAstro/noro-player-mac/releases/download/v#{version}/NoroPlayer.zip"
   name "Noro Player"
